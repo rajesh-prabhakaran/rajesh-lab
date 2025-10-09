72142ead178f7fed135cbb766c0ed6bd6f2d267d
rajcp1 <rajcp1@gmail.com>
2025-10-09 10:44:48 +0000

    main added

---
+++ terraform/main.tf
@@ -0,0 +1,80 @@
+" {
+  region = "eu-west-2" # London
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "confluence_lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "secrets_access" {
+  name        = "SecretsManagerAccess"
+  description = "Allow Lambda to access Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "secretsmanager:GetSecretValue"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "secrets_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.secrets_access.arn
+}
+
+resource "aws_secretsmanager_secret" "confluence_secret" {
+  name = "confluence_api_credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "confluence_secret_version" {
+  secret_id     = aws_secretsmanager_secret.confluence_secret.id
+  secret_string = jsonencode({
+    base_url = "https://your-domain.atlassian.net",
+    username = "your-email@example.com",
+    api_token = "your-api-token"
+  })
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "confluence_lambda" {
+  function_name = "confluence_page_analytics"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.11"
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      CONFLUENCE_SECRET_NAME = aws_secretsmanager_secret.confluence_secret.name
+    }
+  }
+}
+
