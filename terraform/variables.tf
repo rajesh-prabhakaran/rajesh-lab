4a2dc6e2390b7f59c56ea9c3188878146664e78d
rajcp1 <101368899+rajcp1@users.noreply.github.com>
2025-10-09 13:39:02 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
-variable "aws_region" {
-  default = "us-east-1"
+variable "s3_bucket_name" {
+  description = "Name of the existing S3 bucket containing the Lambda ZIP"
+  type        = string
+}
+
+variable "s3_object_key" {
+  description = "Key (path) to the Lambda ZIP file in the S3 bucket"
+  type        = string
 }
